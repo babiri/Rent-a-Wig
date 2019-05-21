1223e6e8023b72cfabd8bd22341b83773c35babc
Fermin Mata <fermenmata@gmail.com>
2019-05-21 16:13:36 +0100

    validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_05_21_145027) do
 
   # These are extensions that must be enabled in order to support this database
@@ -46,12 +45,10 @@ ActiveRecord::Schema.define(version: 2019_05_21_145027) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "price"
-    t.string "photo"
     t.bigint "user_id"
+    t.string "photo"
     t.index ["user_id"], name: "index_wigs_on_user_id"
   end
 
-  add_foreign_key "bookings", "users"
-  add_foreign_key "bookings", "wigs"
   add_foreign_key "wigs", "users"
 end
