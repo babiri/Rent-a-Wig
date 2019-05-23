c36609d95a94cfd982dd441ef91f478d36e61601
Pedro Silva <pedrofjrsilva@gmail.com>
2019-05-23 16:09:40 +0100

    added dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,10 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    @user = current_user
+    @my_wigs = current_user.wigs
+    @my_bookings = current_user.bookings
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'wigs#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :wigs
-  resources :bookings
+  resources :wigs do
+    resources :bookings, only: [:new,:create]
+  end
+
+  get '/dashboard', to: 'pages#dashboard'
 end
