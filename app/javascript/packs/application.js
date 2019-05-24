4f59daee65c54f885f4f023f1bad0c43bb0fc354
Pedro Silva <pedrofjrsilva@gmail.com>
2019-05-24 16:35:30 +0100

    fixed date-picker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,3 @@
 import "bootstrap";
+import "flatpickr/dist/flatpickr.min.css" // Note this is important!import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import "../plugins/flatpickr";
